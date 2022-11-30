7eabd372ee5392f33a0e644aaa87bc991b7edb37
lightlitebug <light.lite.bug@gmail.com>
2022-11-30 10:59:01 +0900

    Combining reducers using TypedReducer

--- lib/redux/item_list/item_list_reducer.dart
+++ lib/redux/item_list/item_list_reducer.dart
@@ -1,14 +1,26 @@
+import 'package:redux/redux.dart';
+
 import 'item_list_action.dart';
 import 'item_list_state.dart';
 
-ItemListState itemsReducer(ItemListState state, dynamic action) {
-  if (action is AddItemAction) {
-    final newItem = action.item;
-    final newItems = [...state.items, newItem];
-    return state.copyWith(items: newItems);
-  } else if (action is DeleteItemAction) {
-    final newItems = state.items.where((item) => item != action.item).toList();
-    return state.copyWith(items: newItems);
-  }
-  return state;
+Reducer<ItemListState> itemsReducer = combineReducers<ItemListState>([
+  TypedReducer<ItemListState, AddItemAction>(addItemReducer),
+  TypedReducer<ItemListState, DeleteItemAction>(deleteItemReducer),
+]);
+
+ItemListState addItemReducer(
+  ItemListState state,
+  AddItemAction action,
+) {
+  final newItem = action.item;
+  final newItems = [...state.items, newItem];
+  return state.copyWith(items: newItems);
+}
+
+ItemListState deleteItemReducer(
+  ItemListState state,
+  DeleteItemAction action,
+) {
+  final newItems = state.items.where((item) => item != action.item).toList();
+  return state.copyWith(items: newItems);
 }
--- lib/redux/search_term/search_term_reducer.dart
+++ lib/redux/search_term/search_term_reducer.dart
@@ -1,9 +1,15 @@
+import 'package:redux/redux.dart';
+
 import 'search_term_action.dart';
 import 'search_term_state.dart';
 
-SearchTermState searchTermReducer(SearchTermState state, dynamic action) {
-  if (action is SearchItemsAction) {
-    return state.copyWith(searchTerm: action.searchTerm);
-  }
-  return state;
+Reducer<SearchTermState> searchTermReducer = combineReducers([
+  TypedReducer<SearchTermState, SearchItemsAction>(searchItemsReducer),
+]);
+
+SearchTermState searchItemsReducer(
+  SearchTermState state,
+  SearchItemsAction action,
+) {
+  return state.copyWith(searchTerm: action.searchTerm);
 }
