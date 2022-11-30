6545ac5ac8bbc37bb1097565f17bddb726c4de39
lightlitebug <light.lite.bug@gmail.com>
2022-11-30 09:33:08 +0900

    Created actions, reducer and inject store into widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
 
 import 'pages/items_page.dart';
+import 'redux/app_reducer.dart';
+import 'redux/app_state.dart';
+
+late final Store<AppState> store;
 
 void main() {
+  store = Store<AppState>(
+    reducer,
+    initialState: AppState.initial(),
+  );
   runApp(const MyApp());
 }
 
@@ -11,12 +21,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return StoreProvider<AppState>(
+      store: store,
+      child: MaterialApp(
+        title: 'Combine Reducers',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const ItemsPage(),
       ),
-      home: const ItemsPage(),
     );
   }
 }
